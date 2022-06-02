da9b84181b86fee17a7de08f0c3710393276fffc
Denis Glotov <denis@glotov.org>
2022-06-02 20:20:03 +0200

    Comments

--- eth-filter-logs/contracts/Emitter.sol
+++ eth-filter-logs/contracts/Emitter.sol
@@ -3,16 +3,16 @@ pragma solidity 0.8.10;
 
 
 contract Emitter {
-    event A(uint256 indexed a);
-    event B(uint256 indexed b1, uint256 b2);
-    event C(uint256 indexed c1, string c2);
-    event D(uint256 indexed d1, uint8 indexed d2, string d3);
+    event Alfa(uint256 indexed a);
+    event Bravo(uint256 indexed b1, uint256 b2);
+    event Charlie(uint256 indexed c1, string c2);
+    event Delta(uint256 indexed d1, uint8 indexed d2, string d3);
 
     function send() public {
-        emit A(1);
-        emit B(2, 22);
-        emit B(3, 33);
-        emit C(4, "44");
-        emit D(5, 55, "555");
+        emit Alfa(1);
+        emit Bravo(2, 22);
+        emit Bravo(3, 33);
+        emit Charlie(4, "44");
+        emit Delta(5, 55, "555");
     }
 }
--- eth-filter-logs/tests/test_emit.py
+++ eth-filter-logs/tests/test_emit.py
@@ -11,23 +11,30 @@ def emitter(Emitter, accounts):
 
 
 def test_emit(accounts, emitter, web3, Emitter):
+    # Invoke send() method of Emitter contract instance
     tx = emitter.send({'from': accounts[0]})
-
     assert len(tx.events) == 5
+
+    # Prepare filter for 3 events (out of 4 emitted)
     filt = web3.eth.filter(
         {
             'address': emitter.address,
             'fromBlock': tx.block_number,
             'toBlock': tx.block_number,
             'topics': [[
-                web3.keccak(text="A(uint256)").hex(),
-                web3.keccak(text="B(uint256,uint256)").hex(),
-                web3.keccak(text="D(uint256,uint8,string)").hex(),
+                web3.keccak(text="Alfa(uint256)").hex(),
+                web3.keccak(text="Bravo(uint256,uint256)").hex(),
+                web3.keccak(text="Delta(uint256,uint8,string)").hex(),
             ]]
         }
     )
+
+    # Query the desiered logs
     logs = web3.eth.get_filter_logs(filt.filter_id)
 
+    # Parse them using web3 log parsers
+    # This O(n^2) approach is not optimal, I didn't find a simple way to
+    # locate contract.event by its topic[0]. Although it would be faster
     contract = web3.eth.contract(abi=Emitter.abi)
     parsed_logs = []
     for event in logs:
@@ -38,6 +45,7 @@ def test_emit(accounts, emitter, web3, Emitter):
             except exceptions.MismatchedABI:
                 pass
 
+    # Pretty print the result
     with open("out.txt", "w") as out:
         out.write(json.dumps(parsed_logs, cls=JsonEthereumEncoder, indent=4))
 
